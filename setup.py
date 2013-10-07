a3c46e51674e778da3994298beac542928adcb53
Mark Levy <mark.levy@mendeley.com>
2013-10-07 15:45:26 +0100

    update setup.py for pypi

--- setup.py
+++ setup.py
@@ -2,13 +2,24 @@
 
 from setuptools import setup, find_packages
 
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(packages=find_packages(),
       version='0.1.2',
       maintainer='Mark Levy',
       name='mrec',
       package_dir={'':'.'},
       maintainer_email='mark.levy@mendeley.com',
-      description='recommender systems library',
+      description='mrec recommender systems library',
+      long_description=long_description,
+      url='https://github.com/mendeley/mrec',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: Unix',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering',],
       install_requires=['numpy','scipy','scikit-learn','ipython'],
       entry_points={
           'console_scripts':[

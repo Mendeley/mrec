83fd8394af66e24d75ade47d5234e906b0e362ce
afefelov <reg@afefelov.ru>
2015-01-14 17:41:02 +0500

    feature (include seen): include already seen items in list

--- mrec/popularity.py
+++ mrec/popularity.py
@@ -8,7 +8,9 @@ import numpy as np
 from base_recommender import BaseRecommender
 from sparse import fast_sparse_matrix
 
+
 class ItemPopularityRecommender(BaseRecommender):
+
     """
     Create an unpersonalized item popularity recommender, useful
     to provide a baseline for comparison with a "real" one.
@@ -28,14 +30,14 @@ class ItemPopularityRecommender(BaseRecommender):
         popularity.
     """
 
-    def __init__(self,method='count',thresh=0):
+    def __init__(self, method='count', thresh=0):
         self.description = 'ItemPop'
-        if method not in ['count','sum','avg','thresh']:
+        if method not in ['count', 'sum', 'avg', 'thresh']:
             raise ValueError('invalid value for method parameter')
         self.method = method
         self.thresh = thresh
 
-    def fit(self,dataset,item_features=None):
+    def fit(self, dataset, item_features=None):
         """
         Compute the most popular items using the method specified
         in the constructor.
@@ -47,26 +49,27 @@ class ItemPopularityRecommender(BaseRecommender):
         item_features : array_like, shape = [num_items, num_features]
             Features for items in training set, ignored here.
         """
-        if isinstance(dataset,fast_sparse_matrix):
+        if isinstance(dataset, fast_sparse_matrix):
             d = dataset.X.tocsc()
         else:
             d = dataset.tocsc()
         if self.method == 'count':
             # count the total number of ratings for each item
-            popularity = [(d[:,i].nnz,i) for i in xrange(d.shape[1])]
+            popularity = [(d[:, i].nnz, i) for i in xrange(d.shape[1])]
         elif self.method == 'sum':
             # find the sum of the ratings for each item
-            popularity = [(d[:,i].sum(),i) for i in xrange(d.shape[1])]
+            popularity = [(d[:, i].sum(), i) for i in xrange(d.shape[1])]
         elif self.method == 'avg':
             # find the mean rating for each item
-            popularity = [(d[:,i].mean(),i) for i in xrange(d.shape[1])]
+            popularity = [(d[:, i].mean(), i) for i in xrange(d.shape[1])]
         elif self.method == 'thresh':
             # count the number of ratings above thresh for each item
-            popularity = [(sum(d[:,i].data>self.thresh),i) for i in xrange(d.shape[1])]
+            popularity = [(sum(d[:, i].data > self.thresh), i)
+                          for i in xrange(d.shape[1])]
         popularity.sort(reverse=True)
-        self.pop_items = [(i,c) for (c,i) in popularity]
+        self.pop_items = [(i, c) for (c, i) in popularity]
 
-    def recommend_items(self,dataset,u,max_items=10,return_scores=True,item_features=None):
+    def recommend_items(self, dataset, u, max_items=10, return_scores=True, item_features=None):
         """
         Recommend new items for a user.  Assumes you've already called
         fit().
@@ -91,16 +94,15 @@ class ItemPopularityRecommender(BaseRecommender):
             List of (idx,score) pairs if return_scores is True, else
             just a list of idxs.
         """
-        known_items = set(dataset[u].indices)
+        #known_items = set(dataset[u].indices)
         recs = []
-        for i,c in self.pop_items:
-            if i not in known_items:
-                if return_scores:
-                    recs.append((i,c))
-                else:
-                    recs.append(i)
-                if len(recs) >= max_items:
-                    break
+        for i, c in self.pop_items:
+            if return_scores:
+                recs.append((i, c))
+            else:
+                recs.append(i)
+            if len(recs) >= max_items:
+                break
         return recs
 
     def __str__(self):
